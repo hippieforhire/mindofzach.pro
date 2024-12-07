5d1bb866ef97d3b59993502c5006d8d55acf98e8
Zachary <zachrog2009@gmail.com>
2024-12-06 19:49:41 -0600

    Update rockpaperscissors.js

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -5,15 +5,15 @@ function playRockPaperScissors() {
     let result = "";
 
     if (userChoice === computerChoice) {
-        result = "It's a draw!";
+        result = "Wow you tied with a computer. Thats pretty lame";
     } else if (
         (userChoice === "Rock" && computerChoice === "Scissors") ||
         (userChoice === "Paper" && computerChoice === "Rock") ||
         (userChoice === "Scissors" && computerChoice === "Paper")
     ) {
-        result = "You win!";
+        result = "Okay, you won. Now go find something else to play.";
     } else {
-        result = "You lose!";
+        result = "Okay so that's embarassing...you lose";
     }
 
     alert(`You chose ${userChoice}, computer chose ${computerChoice}. ${result}`);
