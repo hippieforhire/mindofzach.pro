f5c415a5d7f95a9282ddae9ff2254505e4ffc30e
Zachary <zachrog2009@gmail.com>
2024-12-09 11:30:18 -0600

    Update zork.js

--- js/zork.js
+++ js/zork.js
@@ -42,185 +42,219 @@ const gameData = {
             next: { east: "forest" },
         },
     },
-    // Added more synonyms at the end of each array to greatly expand vocabulary
     synonyms: {
+        // Below are your existing synonyms plus the previously added expansions
+        // I will add even more synonyms at the end of each array for even broader recognition
+        // without removing or changing any existing ones.
         look: [
             "look","examine","inspect","view","peer","see","observe","check","glance","stare","gaze","scrutinize",
             "study","scan","perceive","watch","consider","survey","have a look","take a look","look around",
             "examine surroundings","inspect area","scan room","check place","survey surroundings","examine room",
             "look closely","look carefully","look here","what do i see","describe area","inspect environment",
-            // Additional synonyms:
-            "look over","examine carefully","check out","look upon","eye the area","look about","inspect surroundings"
+            "look over","examine carefully","check out","look upon","eye the area","look about","inspect surroundings",
+            // Additional even more:
+            "look around carefully","look thoroughly","examine in detail","look here closely","i look around","i examine this place"
         ],
         take: [
             "take","grab","pick","take x","take the x","take the","pick up","seize","collect","acquire","obtain",
             "get hold of","snatch","remove","lift","get","claim","retrieve","fetch","steal","pluck","harvest","bag",
             "pocket","add to inventory","take item","get item","yank","pull","hoard","pickup","scoop up","gather",
-            // Additional synonyms for take (including past tense and variations):
             "took","i took","i take","have taken","i have taken","grabbed","pick it up","pick that up","snatched",
-            "i grabbed","i got","i obtained"
+            "i grabbed","i got","i obtained",
+            // More synonyms:
+            "i pick it","i pick it up now","take that item","grab that thing","add that to my inventory","acquire that"
         ],
         attack: [
             "attack","fight","hit","strike","punch","stab","slash","swing at","assault","bash","smack","clobber",
             "whack","beat up","engage","pummel","thrash","clout","hammer","lunge at","charge at","go after","kick",
             "headbutt","wrestle","duel","clash with","swing sword","swing weapon","use weapon","smite","jab",
             "cut","slice","bash skull","try to kill",
-            // Additional synonyms for attack:
             "i attack","i hit","i strike","i fight","go on the offensive","launch an attack","throw a punch",
-            "slash at","stab at","hack at"
+            "slash at","stab at","hack at",
+            // More synonyms:
+            "i swing at the enemy","i try to harm it","i assault it","i attempt to kill this enemy","i try to strike"
         ],
         north: [
             "north","go north","move north","head north","proceed north","travel north","northward","to north",
             "venture north","walk north","step north","head up","go upwards","north direction",
-            // Additional:
-            "i go north","i head north","move upward","northwards","go toward north"
+            "i go north","i head north","move upward","northwards","go toward north",
+            // More synonyms:
+            "proceed upwards","move in northern direction","advance north"
         ],
         south: [
             "south","go south","move south","head south","proceed south","travel south","southward","to south",
             "venture south","walk south","step south","head down","go downwards","south direction",
-            // Additional:
-            "i go south","i head south","move downward","southwards","go toward south"
+            "i go south","i head south","move downward","southwards","go toward south",
+            // More synonyms:
+            "move in southern direction","proceed southwards","advance south"
         ],
         east: [
             "east","go east","move east","head east","proceed east","travel east","eastward","to east",
             "venture east","walk east","step east","east direction","go right way",
-            // Additional:
-            "i go east","i head east","eastwards","move right","go towards east"
+            "i go east","i head east","eastwards","move right","go towards east",
+            // More synonyms:
+            "move in eastern direction","proceed eastwards","advance east"
         ],
         west: [
             "west","go west","move west","head west","proceed west","travel west","westward","to west",
             "venture west","walk west","step west","west direction","go left way",
-            // Additional:
-            "i go west","i head west","westwards","move left","go towards west"
+            "i go west","i head west","westwards","move left","go towards west",
+            // More synonyms:
+            "move in western direction","proceed westwards","advance west"
         ],
         up: [
             "up","go up","move up","climb up","head up","ascend","upward","go upward","go upstairs","go upward path",
-            // Additional:
-            "i go up","i ascend","move upwards","head upwards","ascend upward"
+            "i go up","i ascend","move upwards","head upwards","ascend upward",
+            // More synonyms:
+            "climb upwards","advance upwards","go higher"
         ],
         down: [
             "down","go down","move down","climb down","descend","downward","go downward","go downstairs","go downward path",
-            // Additional:
-            "i go down","i descend","move downwards","head downwards"
+            "i go down","i descend","move downwards","head downwards",
+            // More synonyms:
+            "move lower","go beneath","descend lower"
         ],
         left: [
             "left","go left","move left","step left","head left","turn left","veer left","to the left",
-            // Additional:
-            "i go left","i move left","move to the left side","head to the left"
+            "i go left","i move left","move to the left side","head to the left",
+            // More synonyms:
+            "proceed left","advance left","go in left direction"
         ],
         right: [
             "right","go right","move right","step right","head right","turn right","veer right","to the right",
-            // Additional:
-            "i go right","i move right","move to the right side","head to the right"
+            "i go right","i move right","move to the right side","head to the right",
+            // More synonyms:
+            "proceed right","advance right","go in right direction"
         ],
         back: [
             "back","go back","move back","head back","return","go backward","go backwards","go behind","retreat",
-            // Additional:
-            "i go back","i return","move backwards","head backward","retreat back"
+            "i go back","i return","move backwards","head backward","retreat back",
+            // More synonyms:
+            "go back where I came from","step back","move backward"
         ],
         forward: [
             "forward","go forward","move forward","head forward","proceed forward","advance","go forth","go straight",
-            // Additional:
-            "i go forward","i move forward","i advance","move straight ahead","go straight ahead"
+            "i go forward","i move forward","i advance","move straight ahead","go straight ahead",
+            // More synonyms:
+            "go straight on","proceed ahead","continue forward"
         ],
         run: [
             "run","sprint","dash","bolt","hurry","rush","flee","escape","leg it","make a run for it","race",
             "charge away","hightail","scurry","scuttle","beat it","clear out","jog","run away","run off","run forward",
-            // Additional:
-            "i run","i dash","i sprint","take off running","run quickly","run fast"
+            "i run","i dash","i sprint","take off running","run quickly","run fast",
+            // More synonyms:
+            "i flee","i hurry away","i scurry off","i sprint away","i move fast"
         ],
         jump: [
             "jump","leap","hop","bounce","spring","vault","bound","skip",
-            // Additional:
-            "i jump","i leap","i hop","i bounce","i spring"
+            "i jump","i leap","i hop","i bounce","i spring",
+            // More synonyms:
+            "i vault","i bound","i skip around","i try jumping"
         ],
         climb: [
             "climb","scaling","go climbing","scramble up","clamber","ascend using hands","climb upwards",
-            // Additional:
-            "i climb","i scramble","i clamber up","i scale","attempt to climb"
+            "i climb","i scramble","i clamber up","i scale","attempt to climb",
+            // More synonyms:
+            "i try to climb up","i try scaling","i climb carefully","climb higher"
         ],
         crawl: [
             "crawl","creep","go on all fours","slither","move low","crawl around","crawl slowly",
-            // Additional:
-            "i crawl","i creep","move lowly","go crawling","i slither"
+            "i crawl","i creep","move lowly","go crawling","i slither",
+            // More synonyms:
+            "i crawl around","i creep along","i slink","i move on hands and knees"
         ],
         duck: [
             "duck","crouch","bend down","lower yourself","kneel","stoop","hunch down",
-            // Additional:
-            "i duck","i crouch","i kneel","i lower myself","bend low"
+            "i duck","i crouch","i kneel","i lower myself","bend low",
+            // More synonyms:
+            "i stoop down","i hunch down","i get low","i duck low"
         ],
         dance: [
             "dance","boogie","groove","shake it","jig","twirl","waltz","tap dance","breakdance",
-            // Additional:
-            "i dance","i boogie","i groove","i shake it","start dancing","dance around"
+            "i dance","i boogie","i groove","i shake it","start dancing","dance around",
+            // More synonyms:
+            "i do a jig","i waltz","i breakdance","i dance happily"
         ],
         pickNose: [
             "pick nose","pick your nose","dig nose","nose pick","clean nostril","dig in nostril","extract booger",
-            // Additional:
-            "i pick my nose","i dig my nose","i clean my nostril","nose digging"
+            "i pick my nose","i dig my nose","i clean my nostril","nose digging",
+            // More synonyms:
+            "i pick my nostril","i remove booger","i do something gross","i pick at my nose"
         ],
         sing: [
             "sing","croon","hum a tune","belt out a song","serenade","chant","warble","carol",
-            // Additional:
-            "i sing","i hum","i croon","i belt out a song","start singing"
+            "i sing","i hum","i croon","i belt out a song","start singing",
+            // More synonyms:
+            "i serenade","i chant","i warble","i sing loudly","i try singing"
         ],
         yell: [
             "yell","shout","scream","call out","holler","bellow","cry out","roar",
-            // Additional:
-            "i yell","i shout","i scream","i holler","i roar","yell loudly","shout out loud"
+            "i yell","i shout","i scream","i holler","i roar","yell loudly","shout out loud",
+            // More synonyms:
+            "i bellow","i call out loudly","i cry out","i make noise","i scream loudly"
         ],
         inventory: [
             "inventory","inv","show inventory","what do i have","check bag","check pouch","items on me","my stuff",
             "my items","open inventory","open bag","examine inventory","list items","what am i carrying",
-            // Additional:
-            "i check my stuff","check my items","show me what i have","my inventory","show my inventory","look in my bag"
+            "i check my stuff","check my items","show me what i have","my inventory","show my inventory","look in my bag",
+            // More synonyms:
+            "what am i holding","what is in my inventory","view my items","display my stuff","check what i got"
         ],
         help: [
             "help","instructions","what can i do","commands","assist","how to play","guide","explain","show help",
             "help me","what are my options","i need help",
-            // Additional:
-            "i need instructions","give me help","tell me what i can do","how do i play","help please"
+            "i need instructions","give me help","tell me what i can do","how do i play","help please",
+            // More synonyms:
+            "show commands","give guidance","explain controls","need assistance","what are my choices"
         ],
         use: [
             "use","utilize","apply","wield","equip","put on","activate","operate","make use of","handle",
             "use item","use weapon","use tool","employ",
-            // Additional:
-            "i use","i equip","i apply","i wield","try using","try equipping","attempt to use","attempt to equip"
+            "i use","i equip","i apply","i wield","try using","try equipping","attempt to use","attempt to equip",
+            // More synonyms:
+            "i try to use","i try to equip","i operate this","i make use of it","i attempt to handle"
         ],
         equip: [
             "equip","arm","wield","hold weapon","ready weapon","prepare sword","draw weapon","put weapon in hand",
-            // Additional:
-            "i equip","i arm myself","i wield it","i hold my weapon","i ready my weapon","i draw my blade","i prepare my sword"
+            "i equip","i arm myself","i wield it","i hold my weapon","i ready my weapon","i draw my blade","i prepare my sword",
+            // More synonyms:
+            "i get my weapon ready","i arm up","i equip my gear","i hold my sword tightly"
         ],
         talk: [
             "talk","speak","converse","chat","address","greet","say hello","ask","question","communicate","engage in dialogue",
-            // Additional:
-            "i talk","i speak","i converse","talk to npc","speak to npc","talk with them","speak with them","have a conversation"
+            "i talk","i speak","i converse","talk to npc","speak to npc","talk with them","speak with them","have a conversation",
+            // More synonyms:
+            "i say something","i try to talk","i greet them","i attempt to communicate","i ask them something"
         ],
         trade: [
             "trade","barter","exchange","offer","negotiate","deal","swap",
-            // Additional:
-            "i trade","i barter","i exchange","make a deal","attempt to trade","attempt to barter"
+            "i trade","i barter","i exchange","make a deal","attempt to trade","attempt to barter",
+            // More synonyms:
+            "i try to trade","i offer an exchange","i negotiate","i attempt a deal"
         ],
         give: [
             "give","offer item","hand over","present","deliver","donate","contribute",
-            // Additional:
-            "i give","i offer","i hand over","i present","i deliver","i donate","i contribute","try giving","try offering"
+            "i give","i offer","i hand over","i present","i deliver","i donate","i contribute","try giving","try offering",
+            // More synonyms:
+            "i give them something","i hand something over","i offer this item","i provide this object"
         ],
         eat: [
             "eat","consume","devour","bite","taste","chew","ingest",
-            // Additional:
-            "i eat","i consume","i devour","i bite","i taste","i chew","i ingest","try eating","start eating"
+            "i eat","i consume","i devour","i bite","i taste","i chew","i ingest","try eating","start eating",
+            // More synonyms:
+            "i try to eat","i nibble","i munch","i swallow","i take a bite"
         ],
         drink: [
             "drink","sip","gulp","quench","swallow","imbibe",
-            // Additional:
-            "i drink","i sip","i gulp","i swallow","try drinking","start drinking","drink some"
+            "i drink","i sip","i gulp","i swallow","try drinking","start drinking","drink some",
+            // More synonyms:
+            "i take a sip","i try to drink","i swallow some","i imbibe","i quench thirst"
         ],
         goto: [
             "go to","approach","move to","walk to","head to","approach the","go towards","move towards",
-            // Additional:
-            "i go to","i approach","i move to","i walk to","i head to","i go towards","move closer","approach that"
+            "i go to","i approach","i move to","i walk to","i head to","i go towards","move closer","approach that",
+            // More synonyms:
+            "i move closer to it","i try approaching","i head toward it","i attempt to approach","go near it"
         ]
     },
     inventory: [],
@@ -275,11 +309,20 @@ function handleZorkInput(event) {
     }
 }
 
+// Added a helper function to more accurately check synonyms as whole words
+function matchesSynonym(input, synonym) {
+    // Check exact word boundaries by adding spaces
+    const spacedInput = " " + input + " ";
+    const spacedSynonym = " " + synonym + " ";
+    return spacedInput.includes(spacedSynonym);
+}
+
 function processCommand(input) {
     let commandRecognized = false;
     for (let action in gameData.synonyms) {
         for (let synonym of gameData.synonyms[action]) {
-            if (input.includes(synonym)) {
+            // Use the improved check to avoid partial matches
+            if (matchesSynonym(input, synonym)) {
                 executeCommand(action, input, gameData.rooms[gameData.currentRoom]);
                 commandRecognized = true;
                 return;
